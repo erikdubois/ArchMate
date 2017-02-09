4c6ce765c9929b6f2d6aecdc662c61cd025c9ee4
Erik Dubois <erik.dubois@gmail.com>
2017-02-09 06:38:27 +0100

    Comment :  on Thu Feb  9 06:38:27 CET 2017

--- installation/100-install-core-software-v1.sh
+++ installation/100-install-core-software-v1.sh
@@ -16,7 +16,7 @@ sudo pacman -S --noconfirm --needed archey3 baobab bleachbit catfish clementine 
 sudo pacman -S --noconfirm --needed darktable dconf-editor
 sudo pacman -S --noconfirm --needed dmidecode 
 sudo pacman -S --noconfirm --needed evince evolution filezilla firefox
-sudo pacman -S --noconfirm --needed galculator geary gimp git gksu glances gnome-disk-utility 
+sudo pacman -S --noconfirm --needed geary gimp git gksu glances gnome-disk-utility 
 sudo pacman -S --noconfirm --needed gnome-font-viewer gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool 
 sudo pacman -S --noconfirm --needed gparted gpick grsync
 sudo pacman -S --noconfirm --needed hardinfo hddtemp hexchat htop 
